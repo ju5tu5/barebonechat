c3a25071e05a928c230333763557dc928050c2ac
ju5tu5 <justus@ju5tu5.nl>
2021-04-14 14:50:25 +0200

    changed some things

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
-var socket = io()
-var messages = document.querySelector('section ul')
-var input = document.querySelector('input')
+let socket = io()
+let messages = document.querySelector('section ul')
+let input = document.querySelector('input')
 
-document.querySelector('form').addEventListener('submit', (event) => {
+document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault()
   if (input.value) {
     socket.emit('message', input.value)
@@ -10,9 +10,7 @@ document.querySelector('form').addEventListener('submit', (event) => {
   }
 })
 
-socket.on('message', function(message) {
-  var element = document.createElement('li')
-  element.textContent = message
-  messages.appendChild(element)
+socket.on('message', message => {
+  messages.appendChild(Object.assign(document.createElement('li'), { textContent: message }))
   messages.scrollTop = messages.scrollHeight
 })
