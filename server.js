1d9562a549332fa9f6fe723fc0b6e4fb2b9433ee
ju5tu5 <justus@ju5tu5.nl>
2022-06-20 11:21:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const path = require('path')
 const io = require('socket.io')(http)
 const port = process.env.PORT || 4242
 
+// Start het longpolling proces, geef io mee
+// setInterval(callApi, 2500, io)
+
 // const historySize = 50
 // let history = []
 
@@ -16,7 +19,7 @@ app.use(express.static(path.resolve('public')))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
-  io.emit('history', history)
+  // io.emit('history', history)
 
   socket.on('message', (message) => {
     // while (history.length > historySize) {
@@ -31,6 +34,10 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 })
+
+// function callApi(io) {
+//   io.emit('message', 'whatever')
+// }
 
 http.listen(port, () => {
   console.log('listening on port ', port)
