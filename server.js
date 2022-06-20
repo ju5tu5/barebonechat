e6aade59fd9dc7d90e6bd1300c14799ca652b0e0
ju5tu5 <justus@ju5tu5.nl>
2022-06-20 11:06:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*
 https://socket.io/get-started/chat
 */
+const { ifError } = require('assert')
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
@@ -8,12 +9,21 @@ const path = require('path')
 const io = require('socket.io')(http)
 const port = process.env.PORT || 4242
 
+// const historySize = 50
+// let history = []
+
 app.use(express.static(path.resolve('public')))
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+  io.emit('history', history)
 
   socket.on('message', (message) => {
+    // while (history.length > historySize) {
+    //   history.shift()
+    // }
+    // history.push(message)
+
     io.emit('message', message)
   })
 
