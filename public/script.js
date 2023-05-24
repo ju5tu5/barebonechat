1e9a0141ded02b1675dc4de0f7f6965cd4bccdec
Justus Sturkenboom <justus@ju5tu5.nl>
2023-05-24 09:03:53 +0200

    Added connection states

--- public/script.js
+++ public/script.js
@@ -2,6 +2,11 @@ let ioServer = io()
 let messages = document.querySelector('section ul')
 let input = document.querySelector('input')
 
+// State messages
+const loadingState = document.querySelector('span.loading')
+const emptyState = document.querySelector('span.empty')
+const errorState = document.querySelector('span.offline')
+
 // Luister naar het submit event
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault()
@@ -16,10 +21,64 @@ document.querySelector('form').addEventListener('submit', (event) => {
   }
 })
 
+// Luister naar de historie van de chat
+ioServer.on('history', (history) => {
+  // Als er geen historie is tonen we de empty state
+  if (history.length === 0) {
+    loadingState.style.display = 'none'
+    emptyState.style.display = 'inline'
+
+    // Er zijn berichten, haal de states weg en loop ze op het scherm
+  } else {
+    loadingState.style.display = 'none'
+    emptyState.style.display = 'none'
+    history.forEach((message) => {
+      addMessage(message)
+    })
+  }
+})
+
 // Luister naar berichten van de server
 ioServer.on('message', (message) => {
+  loadingState.style.display = 'none'
+  emptyState.style.display = 'none'
   addMessage(message)
 })
+
+// Er gaat iets mis bij het verbinden
+ioServer.io.on('error', (error) => {
+  loadingState.style.display = 'none'
+  emptyState.style.display = 'none'
+  errorState.style.display = 'inline'
+})
+
+// Poging om opnieuw te verbinden
+ioServer.io.on('reconnect_attempt', (attempt) => {
+  console.log('attempting reconnection')
+})
+
+// Verbinding geslaagd
+ioServer.io.on('reconnect', (attempt) => {
+  loadingState.style.display = 'none'
+  emptyState.style.display = 'none'
+  errorState.style.display = 'none'
+})
+
+// De server stuurt doorlopend pings om te kijken of de boel online is
+ioServer.io.on('ping'() => {
+  // ...
+})
+
+// Als het reconnecten niet goed gaat
+ioServer.io.on("reconnect_error", (error) => {
+  // ...
+});
+
+// Reconnecten is een aantal keer (reconnectionAttempts) geprobeerd en faalt
+// het reconnecten stopt, misschien handig voor een 'probeer opnieuw' knop.
+ioServer.io.on("reconnect_failed", () => {
+  // ...
+});
 
 /**
  * Impure function that appends a new li item holding the passed message to the
--- server.js
+++ server.js
@@ -8,8 +8,18 @@ import express from 'express'
 
 const app = express()
 const http = createServer(app)
-const ioServer = new Server(http)
+const ioServer = new Server(http, {
+  connectionStateRecovery: {
+    // De tijdsduur voor recovery bij disconnect
+    maxDisconnectionDuration: 2 * 60 * 1000,
+    // Of middlewares geskipped moeten worden bij recovery (ivm login)
+    skipMiddlewares: true,
+  },
+})
 const port = process.env.PORT || 4242
+const historySize = 50
+
+let history = []
 
 // Serveer client-side bestanden
 app.use(express.static(path.resolve('public')))
@@ -19,10 +29,17 @@ ioServer.on('connection', (client) => {
   // Log de connectie naar console
   console.log(`user ${client.id} connected`)
 
+  // Stuur de history
+  client.emit('history', history)
+
   // Luister naar een message van een gebruiker
   client.on('message', (message) => {
-    // Log het ontvangen bericht
-    console.log(`user ${client.id} sent message: ${message}`)
+    // Check de maximum lengte van de historie
+    while (history.length > historySize) {
+      history.shift()
+    }
+    // Voeg het toe aan de historie
+    history.push(message)
 
     // Verstuur het bericht naar alle clients
     ioServer.emit('message', message)
